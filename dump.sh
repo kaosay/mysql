fbfc16508b3d21db5776b256945873fafd8e60a9
kaosay <58497583+kaosay@users.noreply.github.com>
2025-11-18 11:28:54 +0800

    date

--- dump.sh
+++ dump.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 mysqldump  --single-transaction  --quick  --lock-tables=false db_name  >  db_20251118.sql
+
+mysqldump -uroot -p  --single-transaction  --quick  --lock-tables=false db_name  >  db_`date +%Y%m%d%H`.sql
